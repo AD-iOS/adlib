fe9294e8b1edcb8757fb14da5090cc720844459c
iOS-GM <1107154510@qq.com>
2026-04-10 20:21:10 +0800

    from update of AD-iOS, update objc++ sub-treasury, some macros have been changed to prevent conflicts Fri, 10 Apr 2026 20:21:09 +0800 2026-04-10 20:21

--- test/old/test.cc
+++
@@ -1,9 +0,0 @@
-#include "include/ADlibc++.hpp"
-
-int main() {
-    ad::cout << "hello\n"
-             << "help\n"
-             << "world\n"
-             << ad::endl;
-    return 0;
-}
--- test/test.c
+++
@@ -1,7 +0,0 @@
-#define ad_system
-#include <AD/ADlibc.h>
-
-int main() {
-    system("echo 'hello'");
-    return 0;
-}
